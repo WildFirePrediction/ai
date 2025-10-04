c33eec18fd6b33220cbf08a8124c99fd743bd9b5
chaseungjoon <cktmdwns3@gmail.com>
2025-10-04 14:18:41 +0900

    Implement kma api

--- src/config.py
+++ src/config.py
@@ -1,9 +1,23 @@
 import os
-""" Directory paths """
 
+""" Directory paths """
 ROOT_DIR = os.path.dirname(os.getcwd())
 SRC_DIR = os.path.join(ROOT_DIR, "src")
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 NASA_DATA_DIR = os.path.join(DATA_DIR, "NASA")
 NASA_MODIS_DATA_DIR = os.path.join(NASA_DATA_DIR, "MODIS")
 NASA_VIIRS_DATA_DIR = os.path.join(NASA_DATA_DIR, "VIIRS")
+KMA_DATA_DIR = os.path.join(DATA_DIR, "KMA")
+
+""" API KEYS"""
+KMA_WEATHER_TOKEN = os.getenv("KMA_WEATHER_TOKEN")
+
+""" KMA API """
+KMA_AWS_BASE_URL = "https://apihub.kma.go.kr/api/typ01/cgi-bin/url/nph-aws2_min"
+KMA_ENDPOINTS = {
+    1: {"url": KMA_AWS_BASE_URL + f"?authKey={KMA_WEATHER_TOKEN}", "filename": "AWS", "filetype": "csv", "desc": "AWS 매분자료"},
+    2: {"url": KMA_AWS_BASE_URL + f"_cloud?authKey={KMA_WEATHER_TOKEN}", "filename": "AWS_cloud", "filetype": "csv", "desc": "AWS 운고운량"},
+    3: {"url": KMA_AWS_BASE_URL + f"_lst?authKey={KMA_WEATHER_TOKEN}", "filename": "AWS_temp", "filetype": "csv", "desc": "AWS 초상온도"},
+    4: {"url": KMA_AWS_BASE_URL + f"_vis?authKey={KMA_WEATHER_TOKEN}", "filename": "AWS_vis", "filetype": "csv", "desc": "AWS 가시거리"},
+}
+
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from kma_api import get_all_weather_data, get_weather_data
+
+
+
+if __name__=="__main__":
+
+    get_all_weather_data()
