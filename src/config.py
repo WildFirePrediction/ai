7b6c613b7ffa0ef0e6a9ad83f1828e4cdf65b442
chaseungjoon <cktmdwns3@gmail.com>
2025-11-04 17:23:58 +0900

    Tweak data paths

--- src/config.py
+++ src/config.py
@@ -2,16 +2,17 @@ import os
 
 """ Directory paths """
 ROOT_DIR = os.getcwd()
+
 SRC_DIR = os.path.join(ROOT_DIR, "src")
 DATA_DIR = os.path.join(ROOT_DIR, "data")
+INFERENCE_DATA_DIR = os.path.join(DATA_DIR, "inference_data")
+
 NASA_DATA_DIR = os.path.join(DATA_DIR, "NASA")
 NASA_MODIS_DATA_DIR = os.path.join(NASA_DATA_DIR, "MODIS")
 NASA_VIIRS_DATA_DIR = os.path.join(NASA_DATA_DIR, "VIIRS")
+
 KMA_DATA_DIR = os.path.join(DATA_DIR, "KMA")
 NDVI_DATA_DIR = os.path.join(DATA_DIR, "NDVI")
-
-""" Inference data paths """
-INFERENCE_DATA_DIR = os.path.join(DATA_DIR, "inference_data")
 KFS_DATA_DIR = os.path.join(INFERENCE_DATA_DIR, "KFS")
 
 """ API KEYS"""
